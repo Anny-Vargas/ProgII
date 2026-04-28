37241a192145caa7a93910af767d14a9b025b04f
Anny Vargas <vargasannydesiree@gmail.com>
2026-04-28 12:10:36 -0500

    Entrega Laboratorio 2 completo

---
+++ Lab 2.3.py
@@ -0,0 +1,7 @@
+print ("Esto funciona")
+print ("Esto tambien funciona")
+print (3)
+print (358)
+print (3 + 5)
+print (2 * 6)
+print ("Somos", 35, "Compañeros")
---
+++ Lab 2.4.py
@@ -0,0 +1,5 @@
+#Esto es un comentario
+print ("Hello, World!")
+#print ("Hello, World!")
+print ("Saludos a todos")
+ 
